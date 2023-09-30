6f776161a96c98270cc70e59a27333dc679e8cba
mpavic-algebra <matej.pavic@predavaci.algebra.hr>
2023-09-30 15:39:51 +0200

    Update users list on every delete with new get request

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
-import {useNavigate} from 'react-router-dom'
 
 const ListUsers = () => {
 
     const [users, setUsers] = useState()
 
-    const navigate = useNavigate()
-
     useEffect(() => {
+        getUsers()
+    },[])
+
+    const getUsers = () => {
         axios.get("https://kv63mr-3000.csb.app/users")
         .then(res => setUsers(res.data))
-    },[])
+    }
 
     const deleteUser = async (e, id) => {
         e.preventDefault()
         const res = await axios.delete(`https://kv63mr-3000.csb.app/users/${id}`)
         console.log(res.status === 204)
-        if (res.status === 204) window.location.reload(false);
+        if (res.status === 204) getUsers()
     }
 
     return (
